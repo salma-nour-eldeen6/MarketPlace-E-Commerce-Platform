3f65065b16bde8b9fa8599157ba16030b4399eb9
AhmadAlaa1 <ahmadalaa114410@gmail.com>
2025-04-19 13:42:06 +0200

    Added Edit Product Component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import VendorDashboard from "./components/Vendors/VendorDashboard";
 import VendorProducts from "./components/Vendors/Products/VendorProducts";
 import CostumerOrders from "./components/Vendors/CostumerOrders/CostumerOrders";
 import AddProduct from "./components/Vendors/AddProduct/AddProduct";
+import EditProduct from './components/Vendors/EditProduct/EditProduct';
 import Profile from "./components/Vendors/Profile/Profile";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -34,6 +35,7 @@ const App = () => {
         <Route path="/vendor-products" element={<VendorProducts />} />
         <Route path="/costumer-orders" element={<CostumerOrders />} />
         <Route path="/Add-Product" element={<AddProduct />} />
+        <Route path="/edit/:id" element={<EditProduct />} />
         <Route path="/Profile" element={<Profile />} />
         {/* Vendor Section */}
       </Routes>
