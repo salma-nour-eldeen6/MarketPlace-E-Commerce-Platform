ac8e8826dbdd57012b8b9eec70d98779414c6b63
AhmadAlaa1 <ahmadalaa114410@gmail.com>
2025-04-19 13:26:27 +0200

    Added Vendors Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { Routes, Route } from 'react-router-dom';
-import './AppStyles.css';
-import CustomerManager from './components/Customers/CustomerManager';
- 
- 
-import Home from './components/Home';
-import NavigationBar from './components/Navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar';
-import NotFound from './components/Pages/NotFound';
-import AuthPage from './components/Pages/AuthPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { Routes, Route } from "react-router-dom";
+import "./AppStyles.css";
+import CustomerManager from "./components/Customers/CustomerManager";
+
+import Home from "./components/Home";
+import NavigationBar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import NotFound from "./components/Pages/NotFound";
+import AuthPage from "./components/Pages/AuthPage";
+import VendorDashboard from "./components/Vendors/VendorDashboard";
+import VendorProducts from "./components/Vendors/Products/VendorProducts";
+import CostumerOrders from "./components/Vendors/CostumerOrders/CostumerOrders";
+import AddProduct from "./components/Vendors/AddProduct/AddProduct";
+import Profile from "./components/Vendors/Profile/Profile";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const App = () => {
-    return (
-        <div className='app-container'>
- 
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path="/AuthPage" element={<AuthPage />} />
-                <Route path='/customers' element={<CustomerManager />} />
-                <Route path='/customers/:id' element={<CustomerManager />} />
-                {/* <Route path='/products' element={<ProductManager />} />
+  return (
+    <div className="app-container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AuthPage" element={<AuthPage />} />
+        <Route path="/customers" element={<CustomerManager />} />
+        <Route path="/customers/:id" element={<CustomerManager />} />
+        {/* <Route path='/products' element={<ProductManager />} />
                 <Route path='/products/:id' element={<ProductManager />} />
                 <Route path='/orders' element={<OrderManager />} />
                 <Route path='/orders/:id' element={<OrderManager />} /> */}
-                <Route path='*' element={<NotFound />} />
-            </Routes>
-        </div>
-    )
+        <Route path="*" element={<NotFound />} />
+
+        {/* Vendor Section */}
+        <Route path="/vendor-dashboard" element={<VendorDashboard />} />
+        <Route path="/vendor-products" element={<VendorProducts />} />
+        <Route path="/costumer-orders" element={<CostumerOrders />} />
+        <Route path="/Add-Product" element={<AddProduct />} />
+        <Route path="/Profile" element={<Profile />} />
+        {/* Vendor Section */}
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
