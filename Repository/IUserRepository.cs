6d7e1ca5184e9966638096cf8002a8bd7a30f4c4
Esraa Samir <esraasamir118881@gmail.com>
2025-04-27 00:40:29 +0300

    Update IUserRepository.cs delete save refresh token

--- Repository/IUserRepository.cs
+++ Repository/IUserRepository.cs
@@ -6,7 +6,6 @@ namespace IA_marketPlace.Repository
     public interface IUserRepository : IGenericRepository<User>
     {
         Task<bool> UserExistsAsync(string email);
-        Task SaveRefreshTokenAsync(int userId, string refreshToken);
         Task<User> GetUserByEmailAsync(string email);
         Task<User> GetUserByRefreshTokenAsync(string token);
     }
