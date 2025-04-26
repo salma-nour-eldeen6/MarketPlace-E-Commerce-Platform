64beb0cb85e4e7c1719e6118d1ec18dcc35b77a9
AhmadAlaa1 <ahmadalaa114410@gmail.com>
2025-04-26 16:14:29 +0300

    Added AdminDashboard Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./AppStyles.css";
 import CustomerManager from "./components/Customers/CustomerManager";
-import ProductDetails from "./components/Pages/ProductDetails";
-import ShoppingCart from './components/Pages/ShoppingCart';
 
 import Home from "./components/Home";
 import NavigationBar from "./components/Navbar/Navbar";
@@ -15,6 +13,10 @@ import CostumerOrders from "./components/Vendors/CostumerOrders/CostumerOrders";
 import AddProduct from "./components/Vendors/AddProduct/AddProduct";
 import EditProduct from './components/Vendors/EditProduct/EditProduct';
 import Profile from "./components/Vendors/Profile/Profile";
+import AdminDashboard from "./components/Admin/AdminDashboard";
+import UserManagement from "./components/Admin/UserManagement/UserManagement";
+import VendorsApplications from "./components/Admin/VendorsApplications/VendorsApplications";
+import ProductsSubmissions from "./components/Admin/Products Submissions/ProductsSubmissions"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -26,8 +28,6 @@ const App = () => {
         <Route path="/AuthPage" element={<AuthPage />} />
         <Route path="/customers" element={<CustomerManager />} />
         <Route path="/customers/:id" element={<CustomerManager />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<ShoppingCart />} />
         {/* <Route path='/products' element={<ProductManager />} />
                 <Route path='/products/:id' element={<ProductManager />} />
                 <Route path='/orders' element={<OrderManager />} />
@@ -42,6 +42,15 @@ const App = () => {
         <Route path="/edit/:id" element={<EditProduct />} />
         <Route path="/Profile" element={<Profile />} />
         {/* Vendor Section */}
+
+        {/* Admin Section */}
+        <Route path="/admin" element={<AdminDashboard />}>
+          <Route index element={<UserManagement />} /> {/* /admin */}
+          <Route path="users" element={<UserManagement />} /> {/* /admin/users */}
+          <Route path="vendors" element={<VendorsApplications />} /> {/*/admin/vendors*/}
+          <Route path="products" element={<ProductsSubmissions />} /> {/*/admin/products */}
+        </Route>
+        {/* Admin Section */}
       </Routes>
     </div>
   );
