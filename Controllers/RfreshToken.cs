403fe3d604c00f6b56bab41320b910aacc7472e3
malak.sayed <137004944+malaksayed9@users.noreply.github.com>
2025-04-27 20:36:52 +0300

    Update RfreshToken.cs

--- Controllers/RfreshToken.cs
+++ Controllers/RfreshToken.cs
@@ -1,70 +1,31 @@
-using IA_marketPlace.Data; 
-
-using IA_marketPlace.Repository; 
-
-using IA_marketPlace.Services; 
-
-using Microsoft.AspNetCore.Authorization; 
-
-using Microsoft.AspNetCore.Http; 
-
-using Microsoft.AspNetCore.Mvc; 
-
-
+using IA_marketPlace.Data;
+using IA_marketPlace.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IA_marketPlace.Controllers
-
 {
-
     [Route("api/[controller]")]
-
     [ApiController]
-
-    public class RfreshToken : ControllerBase
-
+    public class RfreshTokenController : ControllerBase
     {
-
         private readonly ITokenServices _itokenservices;
-
-
-
-        public RfreshToken(ITokenServices itokenservices)
-
+        public RfreshTokenController(ITokenServices itokenservices)
         {
-
             _itokenservices = itokenservices;
-
         }
-
-
 
         [HttpPost("RefreshToken")]
-
         public async Task<IActionResult> RefreshTokenAsync([FromBody] RefreshTokenRequest refreshTokenRequest)
-
         {
-
             var result = await _itokenservices.RefreshTokenAsync(refreshTokenRequest.Token);
 
-
-
             if (result == null)
-
             {
-
                 return Unauthorized("Expired or Invalid");
-
             }
-
             return Ok(result);
-
         }
-
-
-
     }
-
-
-
 }
 
